213c2feeab298302f397a55aaf1a5d95039d2d5a
unknown <KristaCalleja>
2020-03-11 16:42:38 -0300

    x

--- udemycode.py
+++ udemycode.py
@@ -38,4 +38,4 @@ print(name)
 size_input = input("How big is your house?")
 square_feet = int(size_input)
 square_metres = square_feet / 10.8
-print(f"{square_feet} square feet is {square_metres:.3f} square metres.")
+print(f"{square_feet} square feet is {square_metres:.3f} square mestres.")
